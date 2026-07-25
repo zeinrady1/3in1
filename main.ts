e7464503d189e4db0818d52f6c081f7415ebf913
Zein Rady <czvfyc55yp@privaterelay.appleid.com>
2026-07-24 23:44:06 -0700

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,16 @@
 let best = 99999
 let mode = 0
 basic.showString("3-IN-1")
-basic.forever(function on_forever() {
-    let ax: number;
-    let ay: number;
-    let col: number;
-    let row: number;
-    let t0: number;
-    let rt: number;
-    
+
+basic.forever(function () {
     if (mode == 0) {
         led.plotBarGraph(input.soundLevel(), 160)
     } else if (mode == 1) {
-        ax = input.acceleration(Dimension.X)
-        ay = input.acceleration(Dimension.Y)
-        col = Math.map(ax, -1023, 1023, 0, 4)
+        let ax = input.acceleration(Dimension.X)
+        let ay = input.acceleration(Dimension.Y)
+        let col = Math.map(ax, -1023, 1023, 0, 4)
         col = Math.constrain(Math.round(col), 0, 4)
-        row = Math.map(ay, -1023, 1023, 0, 4)
+        let row = Math.map(ay, -1023, 1023, 0, 4)
         row = Math.constrain(Math.round(row), 0, 4)
         basic.clearScreen()
         led.plot(col, row)
@@ -25,7 +19,6 @@ basic.forever(function on_forever() {
         } else {
             music.ringTone(200 + col * 110 + (4 - row) * 130)
         }
-        
     } else {
         basic.showIcon(IconNames.Asleep)
         basic.pause(Math.randomRange(1000, 3000))
@@ -37,12 +30,12 @@ basic.forever(function on_forever() {
                 # # # # #
                 # # # # #
                 `)
-            t0 = input.runningTime()
-            while (!input.buttonIsPressed(Button.B) && mode == 2) {
+            let t0 = input.runningTime()
+            while (!(input.buttonIsPressed(Button.B)) && mode == 2) {
                 basic.pause(1)
             }
             if (mode == 2) {
-                rt = input.runningTime() - t0
+                let rt = input.runningTime() - t0
                 basic.clearScreen()
                 if (rt < 100) {
                     basic.showString("TOO SOON")
@@ -55,24 +48,19 @@ basic.forever(function on_forever() {
                     basic.showString("BEST")
                     basic.showNumber(best)
                 }
-                
                 basic.pause(500)
             }
-            
         }
-        
     }
-    
 })
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    
+
+input.onButtonPressed(Button.A, function () {
     music.stopAllSounds()
     if (mode == 2) {
         mode = 0
     } else {
         mode = mode + 1
     }
-    
     basic.clearScreen()
     if (mode == 0) {
         basic.showString("S")
@@ -81,5 +69,4 @@ input.onButtonPressed(Button.A, function on_button_pressed_a() {
     } else {
         basic.showString("R")
     }
-    
 })
